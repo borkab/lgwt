20c50489bcaa391023e522e8f5a539eeffa5ce2f
borkab <borbala.botond@gmail.com>
2022-03-12 09:01:46 +0100

    refactor: englishHelloPrefix

--- hellow/main.go
+++ hellow/main.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func main() {
 	name := "Borka"
 	fmt.Println(Hello(name))
 }
 
 func Hello(name string) string {
-	return "Hello, " + name
+	return englishHelloPrefix + name
 }
