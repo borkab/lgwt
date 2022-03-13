437cfefc962faecb8be22fe915d9b755efe4f466
borkab <borbala.botond@gmail.com>
2022-03-13 20:12:44 +0100

    example to test

--- ints/main_test.go
+++ ints/main_test.go
@@ -1,6 +1,9 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
@@ -9,4 +12,10 @@ func TestAdder(t *testing.T) {
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
+}
+
+func ExampleAdd() { //if your code changes so that the example
+	sum := Add(1, 5) //is no longer valid, your build will fail
+	fmt.Println(sum)
+	//Output: 6
 }
