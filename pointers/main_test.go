aca229b27e8d4c48120bbdd7c5f3fd38bb5fd76d
borkab <borbala.botond@gmail.com>
2022-03-31 21:03:08 +0200

    assert... refactor

--- pointers/main_test.go
+++ pointers/main_test.go
@@ -6,28 +6,24 @@ import (
 
 func TestWallet(t *testing.T) {
 
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
-
-		got := wallet.Balance()
-		//fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("Wallet: got %s, want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s, want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 }
