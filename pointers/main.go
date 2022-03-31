fb70d8e6664d2f2447cf46a9e05e7e1fe0d81957
borkab <borbala.botond@gmail.com>
2022-03-31 20:57:29 +0200

    Withdraw function

--- pointers/main.go
+++ pointers/main.go
@@ -1,18 +1,34 @@
 package main
 
+import "fmt"
+
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
 }
 
 func main() {
 
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	//fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int { // w is a copy of whatever we called the method from
+func (w *Wallet) Balance() Bitcoin { // w is a copy of whatever we called the method from
 	return (*w).balance // w.balance is also completely valid because in the struct pointers are automatically dereferenced
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
--- pointers/main_test.go
+++ pointers/main_test.go
@@ -1,19 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
 
-	got := wallet.Balance()
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-	want := 10
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	if got != want {
-		t.Errorf("Wallet: got %d, want %d", got, want)
-	}
+		got := wallet.Balance()
+		//fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("Wallet: got %s, want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
 }
