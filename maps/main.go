3dff935c4dc11c891da5b52cedba54fb91541a1b
borkab <borbala.botond@gmail.com>
2022-04-01 21:05:24 +0200

    assertError()

--- maps/main.go
+++ maps/main.go
@@ -4,7 +4,7 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you are looking for")
+var ErrNotFound = errors.New("could not find the word you were looking for")
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
