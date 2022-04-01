49dede3c16c06cfd16ee29c51f1ad83949877f63
borkab <borbala.botond@gmail.com>
2022-04-01 09:48:46 +0200

    assertNoError

--- pointers/main_test.go
+++ pointers/main_test.go
@@ -9,23 +9,33 @@ func TestWallet(t *testing.T) {
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
+
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("Withdraw", func(t *testing.T) {
+	t.Run("Withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
-		startingBalance := Bitcoin(20)
-		wallet := Wallet{startingBalance}
+
+		wallet := Wallet{Bitcoin(20)}
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertError(t, err, ErrInsufficientFunds)
-		assertBalance(t, wallet, startingBalance)
+		assertBalance(t, wallet, Bitcoin(20))
 	})
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an eror but didn't want one")
+	}
 }
 
 func assertError(t testing.TB, got, want error) {
