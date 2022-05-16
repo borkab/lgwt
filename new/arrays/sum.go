a6f1d7e08de9894f5c268d86da34713e2d31e29f
borkab <borbala.botond@gmail.com>
2022-05-16 09:42:55 +0200

    variadic function SumAll

--- new/arrays/sum.go
+++ new/arrays/sum.go
@@ -9,18 +9,12 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-func SumAll(slice1 []int, slice2 []int) []int {
-	var all []int
-	s1 := 0
-	var n1 int
-	for _, n1 = range slice1 {
-		s1 += n1
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
+	sums = make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
 	}
-	s2 := 0
-	var n2 int
-	for _, n2 = range slice2 {
-		s2 += n2
-	}
-	all = append(all, s1, s2)
-	return all
+	return sums
 }
