50394191aa2ce9e4bb4a7e5ee8cef2d74d1bf0f9
borkab <borbala.botond@gmail.com>
2022-05-16 09:37:36 +0200

    my solution on SumAll

--- new/arrays/sum.go
+++ new/arrays/sum.go
@@ -8,3 +8,19 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(slice1 []int, slice2 []int) []int {
+	var all []int
+	s1 := 0
+	var n1 int
+	for _, n1 = range slice1 {
+		s1 += n1
+	}
+	s2 := 0
+	var n2 int
+	for _, n2 = range slice2 {
+		s2 += n2
+	}
+	all = append(all, s1, s2)
+	return all
+}
