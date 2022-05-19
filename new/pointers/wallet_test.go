ebb07bbc0f4a4715e29033111ebb89eae085b00a
borkab <borbala.botond@gmail.com>
2022-05-19 21:00:53 +0200

    t.Run refactored with assertBalance

--- new/pointers/wallet_test.go
+++ new/pointers/wallet_test.go
@@ -5,32 +5,26 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	t.Run("deposit", func(t *testing.T) {
-		wallet := Wallet{}
 
-		wallet.Deposit(Bitcoin(10))
-
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-		//	fmt.Printf("address of ballance in test is %v \n", &wallet.balance)
-		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("got %d want %d", got, want)
 		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-
 		wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
-		//	fmt.Printf("address of ballance in test is %v \n", &wallet.balance)
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %d want %d", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 }
