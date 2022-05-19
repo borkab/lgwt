d3825cf15983becb4e6cfea1f3dd62de6f88f8c9
borkab <borbala.botond@gmail.com>
2022-05-19 11:39:13 +0200

    refactor on table driven tests' structs

--- new/structs/struct_test.go
+++ new/structs/struct_test.go
@@ -26,19 +26,22 @@ func TestPerimeter(t *testing.T) {
 		})
 	*/
 	perimeterTests := []struct { //we are creating a slice of struct
-		shape Shapes2D
-		want  float64
+		name         string
+		shape        Shapes2D
+		hasPerimeter float64
 	}{
-		{shape: Rectangle{12, 6}, want: 36.0},
-		{shape: Circle{10}, want: 62.83185307179586},
-		{shape: Triangle{12, 6, 17}, want: 35.0},
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasPerimeter: 36.0},
+		{name: "Circle", shape: Circle{10}, hasPerimeter: 62.83185307179586},
+		{name: "Triangle", shape: Triangle{12, 6, 17}, hasPerimeter: 35.0},
 	}
 
 	for _, tt := range perimeterTests { //then we iterate over the slice
-		got := tt.shape.Perimeter()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Perimeter()
+			if got != tt.hasPerimeter {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasPerimeter)
+			}
+		})
 	}
 }
 
@@ -66,19 +69,22 @@ func TestArea(t *testing.T) {
 	*/
 
 	areaTests := []struct { //we are creating a slice of struct
-		shape Shapes2D
-		want  float64
+		name    string
+		shape   Shapes2D
+		hasArea float64
 	}{
-		{shape: Rectangle{12, 6}, want: 72.0},
-		{shape: Circle{10}, want: 314.1592653589793},
-		{shape: Triangle{12, 6, 17}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6, 17}, hasArea: 36.0},
 	}
 
-	for _, tt := range areaTests { //then we iterate over the slice
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 
 }
