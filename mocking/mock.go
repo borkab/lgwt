dd97e894315882d87df5a0bc0138eb1459cf7b0d
borkab <borbala.botond@gmail.com>
2022-11-25 20:31:03 +0100

    .

--- mocking/mock.go
+++ mocking/mock.go
@@ -64,3 +64,20 @@ func main() {
 	sleeper := &DefaultSleeper{}
 	Countdown(os.Stdout, sleeper)
 }
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
