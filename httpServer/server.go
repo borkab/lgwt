694b1587c2b134642aee3a753ec677c87fb5076a
borkab <borbala.botond@gmail.com>
2023-02-10 21:20:18 +0100

    lea

--- httpServer/server.go
+++ httpServer/server.go
@@ -9,6 +9,7 @@ import (
 // PlayerStore stores information about players.
 type PlayerStore interface {
 	GetPlayerScore(name string) int
+	RecordWin(name string)
 }
 
 // PlayerServer is an HTTP interface for player information
@@ -19,10 +20,27 @@ type PlayerServer struct {
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
-	score := p.store.GetPlayerScore(player)
-	if score == 0 {
-		w.WriteHeader(http.StatusNotFound)
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
 	}
-	fmt.Fprint(w, score)
 
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
+	score := p.store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(404)
+	}
+
+	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+
+	p.store.RecordWin(player)
+	w.WriteHeader(202)
 }
